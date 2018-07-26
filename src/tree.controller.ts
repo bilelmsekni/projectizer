2c577210f16c622fdb070d2923f06a5a77505413
mseknibilel <msekni.bilel@gmail.com>
2018-07-26 22:56:47 +0200

    DEV | success 1

--- src/tree.controller.ts
+++ src/tree.controller.ts
@@ -1,7 +1,17 @@
 import { Project } from './project.model';
+import { readJson, writeJson } from 'fs-extra';
+import * as vscode from 'vscode';
 
 export class TreeController {
     updateTree(project: Project): void {
-        console.log(project);
+        const path = project.config.slice(1);
+        readJson(path).then(r => this.hideExcluded(r.exclude));
+    }
+
+    private hideExcluded(patterns: string[]): void {
+        console.log(patterns);
+        const excludes: { [key: string]: boolean } = {};
+        patterns.forEach(p => excludes[p] = true);
+        writeJson(vscode.workspace.rootPath + '\\.vscode\\settings.json', { 'files.exclude': excludes }).then(res => console.log(res));
     }
 }
