58f1f5ec3c2162c96e42a8541ed415eb9d40c0a2
mseknibilel <msekni.bilel@gmail.com>
2018-08-11 19:58:41 +0200

    ENH |  code enhancements

--- src/exclusion.controller.ts
+++ src/exclusion.controller.ts
@@ -15,6 +15,26 @@ export class ExclusionController {
         writeJson(this.settingsPath, settings);
     }
 
+    private analyseExcluded(project: Project): string[] {
+        const appSettings = readJsonSync(`${workspace.rootPath}\\${project.appConfig}`);
+        const tstSettings = readJsonSync(`${workspace.rootPath}\\${project.testConfig}`);
+        const appExclude = appSettings.exclude || [];
+        const tstInclude = tstSettings.include || [];
+        return appExclude.filter((e: string) => !tstInclude.includes(e));
+    }
+
+    private mergePatterns(newPatterns: string[][]): { [key: string]: boolean } {
+        const result: { [key: string]: boolean } = {};
+        if (newPatterns && newPatterns.length > 0) {
+            let intersections = newPatterns[0];
+            for (let i = 1; i < newPatterns.length; i++) {
+                intersections = intersections.filter(v => newPatterns[i].some(p => p === v));
+            }
+            intersections.forEach(p => result[p] = true);
+        }
+        return result;
+    }
+
     private addDependencies(patterns: { [key: string]: boolean }): { [key: string]: object | boolean } {
         const additionalPatterns: { [key: string]: object | boolean } = {};
         const whenCondition = { 'when': '$(basename).ts' };
@@ -30,14 +50,6 @@ export class ExclusionController {
         return additionalPatterns;
     }
 
-    private analyseExcluded(project: Project): string[] {
-        const appSettings = readJsonSync(`${workspace.rootPath}\\${project.appConfig}`);
-        const tstSettings = readJsonSync(`${workspace.rootPath}\\${project.testConfig}`);
-        const appExclude = appSettings.exclude || [];
-        const tstInclude = tstSettings.include || [];
-        return appExclude.filter((e: string) => !tstInclude.includes(e));
-    }
-
     private updateSettings(patterns: { [key: string]: boolean | object }): { [key: string]: any } {
         const settings = this.getCurrentSettings();
         settings[FILES_EXCLUDE] = patterns;
@@ -51,18 +63,6 @@ export class ExclusionController {
 
         }
         return {};
-    }
-
-    private mergePatterns(newPatterns: string[][]): { [key: string]: boolean } {
-        const result: { [key: string]: boolean } = {};
-        if (newPatterns && newPatterns.length > 0) {
-            let intersections = newPatterns[0];
-            for (let i = 1; i < newPatterns.length; i++) {
-                intersections = intersections.filter(v => newPatterns[i].some(p => p === v));
-            }
-            intersections.forEach(p => result[p] = true);
-        }
-        return result;
     }
 
     private get settingsPath(): string {
